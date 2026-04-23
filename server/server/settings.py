b0641aa09dd63d509aac8d06de720e9d4886418c
nowayrm <your.email@example.com>
2026-04-23 15:16:05 +0300

    some updates

--- server/server/settings.py
+++ server/server/settings.py
@@ -183,7 +183,7 @@ TIME_ZONE = 'UTC'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
